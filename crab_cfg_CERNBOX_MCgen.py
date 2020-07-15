afae9ef0acfba7918732d31151aafae84ecc7b86
Kevin Mota Amarilo <kevin.amarilo@cern.ch>
2020-07-15 22:42:57 +0200

    updated crab conf files

--- crab_cfg_CERNBOX_MCgen.py
+++ crab_cfg_CERNBOX_MCgen.py
@@ -2,23 +2,25 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'UpsilonOpenCharm_MC_GS_1'
+#config.General.requestName = 'UpsilonDzero_MC_GS_1'
+config.General.requestName = 'UpsilonDplus_MC_GS_1'
 config.General.workArea = 'crab_projects'
 
 config.section_("JobType")
 config.JobType.pluginName = 'PrivateMC'
-config.JobType.psetName = 'UpsilonOpenCharmToMuMu_LHCb_cfg_GS.py'
+#config.JobType.psetName = 'UpsilonToMuMuDzero_GS_cfg.py'
+config.JobType.psetName = 'UpsilonToMuMuDplus_GS_cfg.py'
 config.JobType.allowUndistributedCMSSW = True
 
 config.section_("Data")
-config.Data.publishDBS = 'phys03'
+config.Data.publication = False
 config.Data.outputPrimaryDataset = 'CRAB_PrivateMC'
 config.Data.splitting = 'EventBased'
-config.Data.unitsPerJob = 100
-NJOBS = 10  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
+config.Data.unitsPerJob = 100000
+NJOBS = 100  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
 config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
-config.Data.publication = True
-config.Data.outputDatasetTag = 'UpsilonOpenCharm_CRAB3_MC_GS_1'
+#config.Data.outputDatasetTag = 'UpsilonDzero_CRAB3_MC_GS_1'
+config.Data.outputDatasetTag = 'UpsilonDplus_CRAB3_MC_GS_1'
 
 config.section_("Site")
 config.Site.storageSite = 'T2_CH_CERNBOX' # Should change to the site that you have write permission
