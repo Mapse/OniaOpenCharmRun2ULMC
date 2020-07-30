241651371e63839b2a2460931a711bc9b342cc26
Mapse Barroso Ferreira Filho <mapse.b@hotmail.com>
2020-07-30 23:17:21 +0200

    Path corrections

--- genSim.sh
+++ genSim.sh
@@ -27,7 +27,7 @@
 ########################################## END OF THE INSTRUCTIONS ###################################################3
 
 # Data parser
-. Thesis/mcConfig.sh
+. mcConfig.sh
 
 # Starts CMS enviroment on lxplus.
 cmsenv
@@ -61,7 +61,7 @@ if [[ "$MonteCarlo_energy" == "7TeV" ]]; then
       echo -e " \n \n -------------------------- Running fragment for D0 meson --------------------------"                   
 
       # Generates the fragment for Ultra legacy Monte Carlo 2017.
-      cmsDriver.py Thesis/MonteCarlo/python/SevenTeVFrag/JpsiToMuMuwithDzero_7TeV.py --fileout file:Thesis/MonteCarlo/GenSimFrags/JpsiToMuMuwithDzero_TeV_GS.root --mc --eventcontent RAWSIM --datatier GEN-SIM --conditions 102X_mc2017_realistic_v8 --beamspot Realistic25ns13TeVEarly2017Collision --step GEN,SIM --geometry DB:Extended --era Run2_2017 --python_filename Thesis/MonteCarlo/GenSimFrags/JpsiToMuMuwithDZero_7TeV_GS.py --no_exec -n $MonteCarlo_numevts
+      cmsDriver.py Thesis/MonteCarlo/python/SevenTeVFrag/JpsiToMuMuwithDzero_7TeV.py --fileout file:Thesis/MonteCarlo/GenSimFrags/JpsiToMuMuwithDzero_TeV_GS.root --mc --eventcontent RAWSIM --datatier GEN-SIM --conditions 106X_mc2017_realistic_v7 --beamspot Realistic25ns13TeVEarly2017Collision --step GEN,SIM --geometry DB:Extended --era Run2_2017 --python_filename Thesis/MonteCarlo/GenSimFrags/JpsiToMuMuwithDZero_7TeV_GS.py --no_exec -n $MonteCarlo_numevts
       # Generates events and gives filter efficency and cross sections
       cmsRun Thesis/MonteCarlo/GenSimFrags/JpsiToMuMuwithDZero_7TeV_GS.py
    
@@ -72,7 +72,7 @@ if [[ "$MonteCarlo_energy" == "7TeV" ]]; then
 
       echo -e " \n \n -------------------------- Running fragment for D+ meson --------------------------"                   
 
-      cmsDriver.py Thesis/MonteCarlo/python/SevenTeVFrag/JpsiToMuMuwithDPlus_7TeV.py --fileout file:Thesis/MonteCarlo/GenSimFrags/JpsiToMuMuwithDPlus_7TeV_GS.root --mc --eventcontent RAWSIM --datatier GEN-SIM --conditions 102X_mc2017_realistic_v8 --beamspot Realistic25ns13TeVEarly2017Collision --step GEN,SIM --geometry DB:Extended --era Run2_2017 --python_filename Thesis/MonteCarlo/GenSimFrags/JpsiToMuMuwithDPlus_7TeV_GS.py --no_exec -n $MonteCarlo_numevts
+      cmsDriver.py Thesis/MonteCarlo/python/SevenTeVFrag/JpsiToMuMuwithDPlus_7TeV.py --fileout file:Thesis/MonteCarlo/GenSimFrags/JpsiToMuMuwithDPlus_7TeV_GS.root --mc --eventcontent RAWSIM --datatier GEN-SIM --conditions 106X_mc2017_realistic_v7 --beamspot Realistic25ns13TeVEarly2017Collision --step GEN,SIM --geometry DB:Extended --era Run2_2017 --python_filename Thesis/MonteCarlo/GenSimFrags/JpsiToMuMuwithDPlus_7TeV_GS.py --no_exec -n $MonteCarlo_numevts
       cmsRun Thesis/MonteCarlo/GenSimFrags/JpsiToMuMuwithDPlus_7TeV_GS.py
    
    # Runs Monte Carlo for production of J/Psi meson associated with D_s+ meson.
@@ -82,7 +82,7 @@ if [[ "$MonteCarlo_energy" == "7TeV" ]]; then
       
       echo -e " \n \n -------------------------- Running fragment for D_s+ meson --------------------------"                   
 
-      cmsDriver.py Thesis/MonteCarlo/python/SevenTeVFrag/JpsiToMuMuwithDPlus_7TeV.py --fileout file:Thesis/MonteCarlo/GenSimFrags/JpsiToMuMuwithDPlus_7TeV_GS.root --mc --eventcontent RAWSIM --datatier GEN-SIM --conditions 102X_mc2017_realistic_v8 --beamspot Realistic25ns13TeVEarly2017Collision --step GEN,SIM --geometry DB:Extended --era Run2_2017 --python_filename Thesis/MonteCarlo/GenSimFrags/JpsiToMuMuwithDPlus_7TeV_GS.py --no_exec -n $MonteCarlo_numevts
+      cmsDriver.py Thesis/MonteCarlo/python/SevenTeVFrag/JpsiToMuMuwithDPlus_7TeV.py --fileout file:Thesis/MonteCarlo/GenSimFrags/JpsiToMuMuwithDPlus_7TeV_GS.root --mc --eventcontent RAWSIM --datatier GEN-SIM --conditions 106X_mc2017_realistic_v7 --beamspot Realistic25ns13TeVEarly2017Collision --step GEN,SIM --geometry DB:Extended --era Run2_2017 --python_filename Thesis/MonteCarlo/GenSimFrags/JpsiToMuMuwithDPlus_7TeV_GS.py --no_exec -n $MonteCarlo_numevts
       cmsRun Thesis/MonteCarlo/GenSimFrags/JpsiToMuMuwithDPlus_7TeV_GS.py
    
    # Runs Monte Carlo for production of J/Psi meson associated with Lambda_c+ meson.
@@ -111,7 +111,7 @@ elif [[ "$MonteCarlo_energy" == "13TeV" ]]; then
       echo -e " \n \n -------------------------- Running fragment for D0 meson --------------------------"
 
       # Generates the fragment for Ultra legacy Monte Carlo 2017.
-      cmsDriver.py Thesis/MonteCarlo/python/ThirteenTeVFrag/JpsiToMuMuwithDzero_13TeV.py --fileout file:JpsiToMuMuwithDZero_13TeV_GS.root --mc --eventcontent RAWSIM --datatier GEN-SIM --conditions 102X_mc2017_realistic_v8 --beamspot Realistic25ns13TeVEarly2017Collision --step GEN,SIM --geometry DB:Extended --era Run2_2017 --python_filename JpsiToMuMuwithDZero_13TeV_GS.py --no_exec -n $MonteCarlo_numevts
+      cmsDriver.py Configuration/GenProduction/python/JpsiToMuMuwithDzero_13TeV.py --fileout file:JpsiToMuMuwithDZero_13TeV_GS.root --mc --eventcontent RAWSIM --datatier GEN-SIM --conditions 106X_mc2017_realistic_v7 --beamspot Realistic25ns13TeVEarly2017Collision --step GEN,SIM --geometry DB:Extended --era Run2_2017 --python_filename JpsiToMuMuwithDZero_13TeV_GS.py --no_exec -n $MonteCarlo_numevts
       # Generates events and gives filter efficency and cross sections
       cmsRun JpsiToMuMuwithDZero_13TeV_GS.py
    
@@ -119,7 +119,7 @@ elif [[ "$MonteCarlo_energy" == "13TeV" ]]; then
    elif [[ "$MonteCarlo_particle" == "D+" ]] || [[ "$MonteCarlo_particle" == "Dplus" ]] || [[ "$MonteCarlo_particle" == "DPlus" ]] || [[ "$MonteCarlo_particle" == "d+" ]] || [[ "$MonteCarlo_particle" == "dplus" ]]; then
       PS1='[\u@\h \W]\ D+ Simulation at 13 TeV: $'
       echo -e " \n \n -------------------------- Running fragment for D+ meson --------------------------"
-      cmsDriver.py Thesis/MonteCarlo/python/ThirteenTeVFrag/JpsiToMuMuwithDPlus_13TeV.py --fileout file:JpsiToMuMuwithDPlus_13TeV_GS.root --mc --eventcontent RAWSIM --datatier GEN-SIM --conditions 102X_mc2017_realistic_v8 --beamspot Realistic25ns13TeVEarly2017Collision --step GEN,SIM --geometry DB:Extended --era Run2_2017 --python_filename JpsiToMuMuwithDPlus_13TeV_GS.py --no_exec -n $MonteCarlo_numevts
+      cmsDriver.py Configuration/GenProduction/python/JpsiToMuMuwithDPlus_13TeV.py --fileout file:JpsiToMuMuwithDPlus_13TeV_GS.root --mc --eventcontent RAWSIM --datatier GEN-SIM --conditions 106X_mc2017_realistic_v7 --beamspot Realistic25ns13TeVEarly2017Collision --step GEN,SIM --geometry DB:Extended --era Run2_2017 --python_filename JpsiToMuMuwithDPlus_13TeV_GS.py --no_exec -n $MonteCarlo_numevts
       cmsRun JpsiToMuMuwithDPlus_13TeV_GS.py
    
    # Runs Monte Carlo for production of J/Psi meson associated with D_s+ meson.
@@ -127,7 +127,7 @@ elif [[ "$MonteCarlo_energy" == "13TeV" ]]; then
       PS1='[\u@\h \W]\ D_s+ Simulation at 13 TeV: $'
 
       echo -e " \n \n -------------------------- Running fragment for D_s+ meson --------------------------"
-      cmsDriver.py Thesis/MonteCarlo/python/ThirteenTeVFrag/JpsiToMuMuwithDSPlus_13TeV.py --fileout file:JpsiToMuMuwithDSPlus_13TeV_GS.root --mc --eventcontent RAWSIM --datatier GEN-SIM --conditions 102X_mc2017_realistic_v8 --beamspot Realistic25ns13TeVEarly2017Collision --step GEN,SIM --geometry DB:Extended --era Run2_2017 --python_filename JpsiToMuMuwithDSPlus_13TeV_GS.py --no_exec -n $MonteCarlo_numevts
+      cmsDriver.py Configuration/GenProduction/python/JpsiToMuMuwithDSPlus_13TeV.py --fileout file:JpsiToMuMuwithDSPlus_13TeV_GS.root --mc --eventcontent RAWSIM --datatier GEN-SIM --conditions 106X_mc2017_realistic_v7 --beamspot Realistic25ns13TeVEarly2017Collision --step GEN,SIM --geometry DB:Extended --era Run2_2017 --python_filename JpsiToMuMuwithDSPlus_13TeV_GS.py --no_exec -n $MonteCarlo_numevts
       cmsRun JpsiToMuMuwithDSPlus_13TeV_GS.py
    
    # Runs Monte Carlo for production of J/Psi meson associated with Lambda_c+ meson.
@@ -135,7 +135,7 @@ elif [[ "$MonteCarlo_energy" == "13TeV" ]]; then
       PS1='[\u@\h \W]\ Lambda_c+ Simulation at 13 TeV: $'
 
       echo -e " \n \n -------------------------- Running fragment for Lambda_c+ meson --------------------------"
-      cmsDriver.py Thesis/MonteCarlo/python/ThirteenTeVFrag/JpsiToMuMuwithLambdaCPlus_13TeV.py --fileout file:JpsiToMuMuwithLambdac_13TeV_GS.root --mc --eventcontent RAWSIM --datatier GEN-SIM --conditions 102X_mc2017_realistic_v7 --beamspot Realistic25ns13TeVEarly2018Collision --step GEN,SIM --geometry DB:Extended --era Run2_2017 --python_filename JpsiToMuMuwithLambdaCPlus_13TeV_GS.py --no_exec -n $MonteCarlo_numevts
+      cmsDriver.py Configuration/GenProduction/python/JpsiToMuMuwithLambdaCPlus_13TeV.py --fileout file:JpsiToMuMuwithLambdac_13TeV_GS.root --mc --eventcontent RAWSIM --datatier GEN-SIM --conditions 102X_mc2017_realistic_v7 --beamspot Realistic25ns13TeVEarly2018Collision --step GEN,SIM --geometry DB:Extended --era Run2_2017 --python_filename JpsiToMuMuwithLambdaCPlus_13TeV_GS.py --no_exec -n $MonteCarlo_numevts
       cmsRun JpsiToMuMuwithLambdaCPlus_13TeV_GS.py
    
    fi
