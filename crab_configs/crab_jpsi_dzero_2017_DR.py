f46c85629eb5cf38a1946d44ab88e899487add90
Mapse Barroso Ferreira Filho <mapse.b@hotmail.com>
2021-06-04 15:21:36 +0200

    Adding old crab configd

--- crab_configs/crab_jpsi_dzero_2017_DR.py
+++ crab_configs/crab_jpsi_dzero_2017_DR.py
@@ -1,14 +1,15 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = 'JpsiDPlus_MC_GS'
+config.General.requestName = 'JpsiDzero_MC_2017_DR'
 config.General.workArea = 'crab_projects'
 
 config.JobType.pluginName = 'PrivateMC'
-config.JobType.psetName = 'JpsiToMuMuwithDPlus_13TeV_2016_GS_cfg.py'
+config.JobType.psetName = 'JpsiToMuMuwithDzero_13TeV_2017_DR_cfg.py'
 config.JobType.allowUndistributedCMSSW = True
+config.JobType.numCores = 4
 
 config.Data.publication = False
 config.Data.outputPrimaryDataset = 'CRAB_PrivateMC'
 config.Data.splitting = 'EventBased'
-config.Data.outputDatasetTag = 'JpsiDPlus_CRAB3_MC_GS'
+config.Data.outputDatasetTag = 'JpsiDzero_CRAB3_MC_2017_DR' 
