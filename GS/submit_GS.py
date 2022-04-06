c572c5d2ef752b54fb1bb45811cb69493559a82b
Mapse Barroso Ferreira Filho <mapse.b@hotmail.com>
2022-04-06 22:57:54 +0200

    naming convening changed

--- GS/submit_GS.py
+++ GS/submit_GS.py
@@ -4,13 +4,13 @@ import datetime
 today = datetime.date.today()
 
 njobs = 10000 # Change for the wanted number of jobs 10000
-evtsjob = 1200000 # Change for the wanted number of evts per job 1200000
+evtsjob = 1000000 # Change for the wanted number of evts per job 1200000
 template = "crab_config_GS"
 
 for config in subprocess.check_output("ls config/", shell=True).decode("utf-8").splitlines(): # 
     if not config.endswith(".py"): # Put only .py for all files
         continue
-    dataset = config[0: config.find("_")]
+    dataset = config[0: config.find("_GS")]
     with open(template + ".py", 'r') as f:
         new_file = f.read().replace("DATASET", dataset)
         new_file = new_file.replace("DATE", today.strftime("%d-%m-%Y"))
